47be9bb745b208e03164a527ce9ac308bb98163f
c483481 <charleskojansow@gmail.com>
2024-07-17 22:55:27 +0800

    feat(broker): add call log request with http

--- broker-service/cmd/api/routesHttp.go
+++ broker-service/cmd/api/routesHttp.go
@@ -27,6 +27,17 @@ func HandleHttpSubmission(c *fiber.Ctx) error {
 		}
 
 		return authenticate(c, &authPayload)
+	case "log":
+		logsPayload := &LogsPayload{
+			Name: payload.Data["name"].(string),
+			Data: payload.Data["data"].(string),
+		}
+		if valid := ValidateStruct(logsPayload); valid != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(valid)
+		}
+
+		return insertLogs(c, logsPayload)
+
 	default:
 		return WrapData(c, "Invalid action")
 	}
@@ -72,4 +83,39 @@ func authenticate(c *fiber.Ctx, payload *AuthPayload) error {
 			Message: "Failed to create request",
 		})
 	}
+}
+
+func insertLogs(c *fiber.Ctx, payload *LogsPayload) error {
+	response, err := SendRequest("POST", "http://logger-services/logs", payload)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(&ErrorResponseType{
+			Success: false,
+			Code:    "E_REQ",
+			Message: err.Error(),
+		})
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return c.Status(fiber.StatusInternalServerError).JSON(&ErrorResponseType{
+			Success: false,
+			Code:    "E_REQ",
+			Message: "Failed to create request",
+		})
+	}
+
+	var jsonResponse AppResponses
+	err = json.NewDecoder(response.Body).Decode(&jsonResponse)
+
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(&ErrorResponseType{
+			Success: false,
+			Code:    "E_REQ",
+			Message: "Failed to create request",
+		})
+	}
+
+	return WrapData(c, jsonResponse.Data)
 }
